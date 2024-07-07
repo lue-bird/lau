8fe7f2989f52705edbf585d624b9aa9e8c7c2b50
lue-bird <lue.the.bird@gmail.com>
2024-07-07 15:03:35 +0200

    switch to vite-plugin-elm to use elm-optimize-level-2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as Web from "@lue-bird/elm-state-interface"
-import Main from "./Main.elm"
+import { Elm } from "./Main.elm"
 
-const elmApp = Main.init()
+const elmApp = Elm.Main.init()
 Web.programStart({ ports: elmApp.ports, domElement: document.getElementById("app") })
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
-import { defineConfig } from 'vite'
-import elm from 'vite-plugin-elm-watch'
+import { defineConfig } from "vite"
+import elm from "vite-plugin-elm"
+import fs from "node:fs"
+import eol2 from "elm-optimize-level-2"
+import { temporaryFile } from "tempy"
 
 export default defineConfig({
-    plugins: [elm()]
+    plugins: [
+        elm({ compiler: { compile: compileWithEOL2 } })
+    ]
 })
+
+const compileWithEOL2 = async (targets) => {
+    const output = temporaryFile({ extension: 'elm' });
+    await eol2.run({
+        inputFilePath: targets,
+        outputFilePath: output,
+        optimizeSpeed: true,
+        processOpts: { stdio: ['inherit', 'ignore', 'inherit'] },
+    })
+    return fs.readFileSync(output).toString()
+}
