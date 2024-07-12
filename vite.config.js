032bfb1568377bd7b788e3f70ab28ceb307100a0
lue-bird <lue.the.bird@gmail.com>
2024-07-12 15:13:24 +0200

    font move to static/fonts

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite"
 import elm from "vite-plugin-elm"
 import eol2 from "elm-optimize-level-2"
 import { devNull } from "node:os"
+import * as path from "node:path"
 import * as esbuild from "esbuild"
 
 // thanks https://discourse.elm-lang.org/t/what-i-ve-learned-about-minifying-elm-code/7632
@@ -13,22 +14,27 @@ export default defineConfig({
                 compile: compile
             }
         })
-    ]
+    ],
+    resolve: {
+        alias: {
+            $fonts: path.resolve('./static/fonts')
+        }
+    }
 })
 
 const compile = (targets) => {
     return compileWithEOL2(targets)
-        .then(eol2Output =>
-            esbuild.transform(
-                removeIife(eol2Output),
-                {
-                    minify: true,
-                    pure: pureFuncs,
-                    target: "es5",
-                    format: "iife"
-                }
-            ).code
-        )
+    //.then(eol2Output =>
+    //    esbuild.transform(
+    //        removeIife(eol2Output),
+    //        {
+    //            minify: true,
+    //            pure: pureFuncs,
+    //            target: "es5",
+    //            format: "iife"
+    //        }
+    //    ).code
+    //)
 }
 
 const pureFuncs = ["F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9"]
