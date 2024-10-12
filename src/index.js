40d5878fbad90709deb594e633594ee1b9145fac
lue-bird <lue.the.bird@gmail.com>
2024-10-12 05:11:47 +0200

    correct setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as Web from "@lue-bird/elm-state-interface-experimental"
-import { Elm } from "./Main.elm"
+import Main from "./Main.elm"
 
-const elmApp = Elm.Main.init()
+const elmApp = Main.init()
 Web.programStart({ ports: elmApp.ports, domElement: document.getElementById("app") })
--- vite.config.js
+++ vite.config.js
@@ -1,56 +1,41 @@
 import { defineConfig } from "vite"
-import elm from "vite-plugin-elm"
-import eol2 from "elm-optimize-level-2"
-import { devNull } from "node:os"
+import elm from "vite-plugin-elm-watch"
+import * as elmOptimizeLevel2 from "elm-optimize-level-2"
 import * as path from "node:path"
-import * as esbuild from "esbuild"
 
-// thanks https://discourse.elm-lang.org/t/what-i-ve-learned-about-minifying-elm-code/7632
-
-export default defineConfig({
+export default defineConfig((environment) => ({
     plugins: [
         elm({
-            compiler: {
-                compile: compile
-            }
-        })
+            mode:
+                environment.command === "build" ?
+                    "optimize"
+                    : "debug"
+
+        }),
+        environment.command === "build" ?
+            applyEOL2()
+            : []
     ],
+    base: "/lau/",
     resolve: {
         alias: {
             $fonts: path.resolve('./static/fonts')
         }
     }
-})
+}))
 
-const compile = (targets) => {
-    return compileWithEOL2(targets)
-    //.then(eol2Output =>
-    //    esbuild.transform(
-    //        removeIife(eol2Output),
-    //        {
-    //            minify: true,
-    //            pure: pureFuncs,
-    //            target: "es5",
-    //            format: "iife"
-    //        }
-    //    ).code
-    //)
-}
+const jsFileRegex = /\.(js)$/
+function applyEOL2() {
+    return {
+        name: 'apply-elm-optimize-level-2',
 
-const pureFuncs = ["F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9"]
-
-const removeIife = (elmJsCode) => {
-    return `var scope = window;${elmJsCode.slice(
-        elmJsCode.indexOf("{") + 1,
-        elmJsCode.lastIndexOf("}")
-    )}`
-}
-
-const compileWithEOL2 = (targets) => {
-    return eol2.run({
-        inputFilePath: targets,
-        outputFilePath: devNull,
-        optimizeSpeed: true,
-        processOpts: { stdio: ["inherit", "ignore", "inherit"] },
-    })
+        async transform(src, id) {
+            if (jsFileRegex.test(id)) {
+                return {
+                    code: await elmOptimizeLevel2.transform(src, true),
+                    map: null
+                }
+            }
+        }
+    }
 }
